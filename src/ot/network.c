b83172c337e7c77dba10fc0729b1a2891d726c20
Amogh Kashyap <94269573+amoghkash@users.noreply.github.com>
2025-02-19 14:32:26 -0500

    Added Server Initialization

--- src/ot/network.c
+++ src/ot/network.c
@@ -10,12 +10,22 @@
 #define TIMEOUT 1000
 
 int initialize_server() {
-    printk("Server initialization");
+    otError error;
+    otInstance *t_instance = openthread_get_default_instance();
+
+    error = otCoapStart(t_instance, OT_DEFAULT_COAP_PORT);
+    if (error != OT_ERROR_NONE) {
+        goto fail;
+    }
+    otCoapAddResource(t_instance, NULL);
     return 0;
+
+fail:
+    return EOT_FAIL;
 }
 
 int initialize_client() {
-    printk("Client initialization");
+    printk("Client initialization\n");
     return 0;
 }
 
